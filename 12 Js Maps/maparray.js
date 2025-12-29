554d1e36634082068a2bdd747dcf00d6f23d4e47
basharatbutt <basharatdev57@gmail.com>
2025-12-29 12:39:01 +0500

    Map Object

---
+++ 12 Js Maps/maparray.js
@@ -0,0 +1,7 @@
+let furites = new Map([
+    ["apples", 400],
+    ["Mango", 300],
+    ["oranges", 200,]
+]);
+let num = furites.get("apples");
+document.getElementById("dora").innerHTML = "There are " + num + " apples"
