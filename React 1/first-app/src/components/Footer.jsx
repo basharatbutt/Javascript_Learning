c3e4cad08e82f7deede0e6d6c3e952b2a57bac7d
basharatbutt <basharatdev57@gmail.com>
2026-01-08 17:47:14 +0500

    React Components, props and jsx

---
+++ React 1/first-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div className='uio'>
+      Copyright code is not | Alowed be Careful
+    </div>
+  )
+}
+
+export default Footer
