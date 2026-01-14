f5a192645421787fab44cb87552e5792fcda137d
basharatbutt <basharatdev57@gmail.com>
2026-01-14 12:09:42 +0500

    Array

---
+++ Javascript Array/array.js
@@ -0,0 +1,11 @@
+let arr= [93787,2,3,5 ,8 ]
+//console.log(arr[0])
+
+const myArr2 = [1, 2, 3, 4]
+myArr2.pop()
+// console.log( myArr2)
+
+console.log("A", myArr2)
+let ma1 = myArr2.slice(1,4)
+console.log(ma1)
+
