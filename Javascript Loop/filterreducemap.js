9205ac0583ae231fad7e2b857aea221147718cb3
basharatbutt <basharatdev57@gmail.com>
2026-01-19 13:49:40 +0500

    Create filterreducemap.js

---
+++ Javascript Loop/filterreducemap.js
@@ -0,0 +1,22 @@
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+  const mybook = books.filter( (special) => special.edition >= 2000  )
+//   console.log(mybook)
+
+let arrnum = [1,2,34,5,9,6,2,3];
+const num = arrnum
+.map(  (add) => add + 1 )
+.map( (multi) => multi * 10 )
+.filter ( (ager) => ager > 32 )
+console.log(num)
+
+
