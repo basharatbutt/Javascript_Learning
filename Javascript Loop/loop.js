a0cfb7c0b2598c856388f7700f123f6604cb0a94
basharatbutt <basharatdev57@gmail.com>
2026-01-19 11:57:50 +0500

    Create loop.js

---
+++ Javascript Loop/loop.js
@@ -0,0 +1,16 @@
+for (let i = 1; i <= 3; i++) {
+    const element = i;
+    // console.log(`outer loop value${i}`)
+    for (let k = 0; k < 15; k++) {
+        const element = k;
+        // console.log(i + "*" + k + "=" + i*k)
+    }
+    
+}
+
+let myArr = ["spiderman", "thro", "hulk"]
+for (let i = 0; i < myArr.length; i++) {
+    const element = myArr[i];
+    console.log(element)
+    
+}
